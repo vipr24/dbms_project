afe3deeeadbcd34602158a7e2a9fba23b3d4c2c5
vipr24 <viprashukla979@gmail.com>
2025-11-03 03:15:05 +0530

    hardcoding login for testing

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,22 @@ app.get('/', (req, res) => {
     res.send("Hi this is server");
 })
 
+// auth routes
+app.post(/login/doctor, (req, res)=>{
+    const {email, password} = req.body;
+    if(email==='doctor1@gmail.com' && password==='doctor123'){
+        res.status(200);
+    }
+})
+app.post(/register/doctor, (req, res)=>{})
+app.post(/login/patient, (req, res)=>{
+    const {email, password} = req.body;
+    if(email==='patient1@gmail.com' && password==='patient123'){
+        res.status(200);
+    }
+})
+app.post(/register/patient, (req, res)=>{})
+
 app.listen(3000, () => {
     console.log("server is running");
 })
