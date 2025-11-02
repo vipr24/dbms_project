f48eff905faba0c97a9322a805106d3b405ba286
vipr24 <viprashukla979@gmail.com>
2025-11-03 02:50:35 +0530

    react-router setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<App />} />
+				<Route path="/doctor" element={<App />} />
+				<Route path="/patient" element={<App />} />
+			</Routes>
+		</BrowserRouter>
+	</StrictMode>
+);
